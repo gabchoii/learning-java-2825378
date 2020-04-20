270368d522f01ee92eab13c4b78d67f7a3be7620
gabchoii <60411269+gabchoii@users.noreply.github.com>
2020-04-20 20:30:36 -0300

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello World! It's me Kathryn!");
+	public static void main (String [] args) {
+		System.out.println("Hello World! It's me Choi!");
+
+
 	}
+
 }
